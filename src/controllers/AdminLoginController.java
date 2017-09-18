cab3479e54996d4e7e5108cec4a0cc1230093841
DESKTOP-88IF2P3\George Stratulat <george.stratulat10@gmail.com>
2017-09-18 11:44:29 +0200

    second commit

--- src/controllers/AdminLoginController.java
+++ src/controllers/AdminLoginController.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
 import models.Admin;
 
-import java.awt.event.ActionEvent;
 
 /**
  * Created by George Stratulat on 18/09/2017.
@@ -28,8 +36,17 @@ public class AdminLoginController {
     }
 
     @FXML
-    public void login(ActionEvent event)throws Exception{
-        if(checkLogin())
+    public void login(javafx.event.ActionEvent event)throws Exception{
+        if(checkLogin(passwordField.getText()) == true){
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("/gui/Random.fxml"));
+            Scene scene = new Scene(root,1024,720);
+            stage.setScene(scene);
+            stage.show();
+
+
+        }
 
     }
 
